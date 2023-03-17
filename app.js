2b75a27801a3266e70aaf7fd32e100cfa54bd8ef
developer-shourav <mrinmoyraj823@gmail.com>
2023-03-17 11:36:20 +0600

    search result not showng bub fixed

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const showImages = (images) => {
 const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
-    .then(data => showImages(data.hitS))
+    .then(data => showImages(data.hits))
     .catch(err => console.log(err))
 }
 
